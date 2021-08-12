dfa3938eacde56cdbcaa913dd039ce084bd6fd97
Lu Zhao <lzhao819@usc.edu>
2021-08-12 15:48:12 +0800

    post put delete positions

--- src/main/java/com/example/fundmanagement/positions/Positions.java
+++ src/main/java/com/example/fundmanagement/positions/Positions.java
@@ -5,43 +5,46 @@ import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
-@Table
+@Table(name = "positions")
 public class Positions {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer positionsId;
+    private Integer position_id;
 
     //@ManyToOne()
-    private String securityName;
+    private String security_name;
     private Integer quantity;
-    private LocalDate datePurchased;
-    private Integer fundId;
+    private LocalDate date_purchased;
+    private Integer funds_fund_id;
+    private Integer funds_fund_manager_employee_id;
+    private Integer securities_security_id;
 
     public Positions() {
     }
 
-    public Positions(Integer positionsId, String securityName, Integer quantity, LocalDate datePurchased, Integer fundId) {
-        this.positionsId = positionsId;
-        this.securityName = securityName;
+    public Positions(Integer position_id, String security_name, Integer quantity, LocalDate date_purchased, Integer funds_fund_id,Integer funds_fund_manager_employee_id,Integer securities_security_id) {
+        this.position_id = position_id;
+        this.security_name = security_name;
         this.quantity = quantity;
-        this.datePurchased = datePurchased;
-        this.fundId = fundId;
+        this.date_purchased = date_purchased;
+        this.funds_fund_id = funds_fund_id;
+        this.funds_fund_manager_employee_id = funds_fund_manager_employee_id;
+        this.securities_security_id = securities_security_id;
     }
 
-    public Integer getPositionsId() {
-        return positionsId;
+    public Integer getPosition_id() {
+        return position_id;
     }
 
-    public void setPositionsId(Integer positionId) {
-        this.positionsId = positionId;
+    public void setPosition_id(Integer positionId) {
+        this.position_id = positionId;
     }
 
-    public String getSecurityName() {
-        return securityName;
+    public String getSecurity_name() {
+        return security_name;
     }
 
-    public void setSecurityName(String securityName) {
-        this.securityName = securityName;
+    public void setSecurity_name(String securityName) {
+        this.security_name = securityName;
     }
 
     public Integer getQuantity() {
@@ -52,30 +55,48 @@ public class Positions {
         this.quantity = quantity;
     }
 
-    public LocalDate getDatePurchased() {
-        return datePurchased;
+    public LocalDate getDate_purchased() {
+        return date_purchased;
     }
 
-    public void setDatePurchased(LocalDate datePurchased) {
-        this.datePurchased = datePurchased;
+    public void setDate_purchased(LocalDate datePurchased) {
+        this.date_purchased = datePurchased;
     }
 
-    public Integer getFundId() {
-        return fundId;
+    public Integer getFunds_fund_id() {
+        return funds_fund_id;
     }
 
-    public void setFundId(Integer fundId) {
-        this.fundId = fundId;
+    public void setFunds_fund_id(Integer fundId) {
+        this.funds_fund_id = fundId;
+    }
+
+    public Integer getFunds_fund_manager_employee_id() {
+        return funds_fund_manager_employee_id;
+    }
+
+    public void setFunds_fund_manager_employee_id(Integer funds_fund_manager_employee_id) {
+        this.funds_fund_manager_employee_id = funds_fund_manager_employee_id;
+    }
+
+    public Integer getSecurities_security_id() {
+        return securities_security_id;
+    }
+
+    public void setSecurities_security_id(Integer securities_security_id) {
+        this.securities_security_id = securities_security_id;
     }
 
     @Override
     public String toString() {
         return "Positions{" +
-                "positionId=" + positionsId +
-                ", securityName='" + securityName + '\'' +
+                "position_id=" + position_id +
+                ", security_name='" + security_name + '\'' +
                 ", quantity=" + quantity +
-                ", datePurchased=" + datePurchased +
-                ", fundId=" + fundId +
+                ", date_purchased=" + date_purchased +
+                ", funds_fund_id=" + funds_fund_id +
+                ", funds_fund_manager_employee_id=" + funds_fund_manager_employee_id +
+                ", securities_security_id=" + securities_security_id +
                 '}';
     }
 }
