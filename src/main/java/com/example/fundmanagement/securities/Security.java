a64da4de020bca7320788cbb379ae510868a0271
Lu Zhao <lzhao819@usc.edu>
2021-08-16 09:14:30 +0800

    positions cascade

--- src/main/java/com/example/fundmanagement/securities/Security.java
+++ src/main/java/com/example/fundmanagement/securities/Security.java
@@ -1,6 +1,10 @@
 package com.example.fundmanagement.securities;
 
+import com.example.fundmanagement.positions.Positions;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name="securities")
@@ -11,11 +15,25 @@ public class Security {
 
     private String symbol;
 
+    @OneToMany(mappedBy = "securityInPosition",cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
+    @JsonIdentityReference(alwaysAsId = true)
+    private List<Positions> positions;
+
     //constructors
-    public Security(Integer security_id, String symbol) {
+    public Security(Integer security_id, String symbol,List<Positions> positions) {
         this.security_id = security_id;
         this.symbol = symbol;
+        this.positions = positions;
     }
+
+    public List<Positions> getPositions() {
+        return positions;
+    }
+
+    public void setPositions(List<Positions> positions) {
+        this.positions = positions;
+    }
+
     public Security(){}
 
     public Integer getSecurity_id() {
